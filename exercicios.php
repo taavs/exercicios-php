1341ecb2dcc7147d0342ed6a58b1fc2007405bf3
Luis Otavio <desenvolvimento07@crmsolucoes.local>
2019-02-18 10:16:08 -0300

    First Commit

---
+++ exercicios.php
@@ -0,0 +1,36 @@
+<?php
+
+/* - Crie uma função que retorna um CPF ou CNPJ formatado com dígitos e traços
+ * - A função deve conter apenas 1 argumento que é uma string
+ * - Faça as tratativas que julgar necessárias para que a função não funcione de maneira inesperada.
+ * - O retorno deve ser uma string formata da maneira que CPFs e CNPJs são formatados.
+ * - Site para gerar cpf https://www.geradordecpf.org/ cpnj:https://www.4devs.com.br/gerador_de_cnpj
+ */
+
+/*
+* - Crie uma função que retorna a data por extenso.
+* - A função deve conter apenas 1 argumento que é uma string no formato 'dd/mm/YYYY'
+* - O retorno deve ser uma string formatada Exemplo: '01 de Janeiro de 2001'
+* - Faça as tratativas que julgar necessárias para que a função não funcione de maneira inesperada.
+*/ 
+
+/*
+* - Crie uma função que encontra o valor mais alto existente em dois arrays de numeros inteiros.
+* - A função deve conter 2 argumentos, sendo eles dois arrays de numeros inteiros. 
+* - O retorno deve ser o número mais alto existente nestes dois arrays que foram passados.
+* - Faça as tratativas que julgar necessárias para que a função não funcione de maneira inesperada.
+*/
+
+/*
+* - Crie uma função que verifica se uma palavra é um palíndromo.
+* - A função deve conter 1 argumento, sendo ele uma string. 
+* - O retorno deve ser booleano.
+* - Dica: Palíndromos são palavras que podem ser lidas de trás para frente. 
+* - Exemplo: Ovo, Ana, Radar, Saias, Reviver etc.
+*/
+
+/*
+* - Crie uma função que lê a quantidade de linhas que existe em um arquivo .txt
+* - A função deve conter 1 argumento, sendo ele o nome do arquivo a ser "calculado".
+* - O retorno deve ser o número de linhas existentes no arquivo .txt
+*/
